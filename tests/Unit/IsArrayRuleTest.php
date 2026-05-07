1c072299defec39a93ec7601f7a781cc2ba69f7e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-05-07 20:25:25 +0400

    [AEGISORA-8] Setup test.

--- tests/Unit/IsArrayRuleTest.php
+++ tests/Unit/IsArrayRuleTest.php
@@ -2,8 +2,17 @@
 
 namespace Aegisora\Rules\Tests\Unit;
 
+use Aegisora\Rules\IsArrayRule;
 use PHPUnit\Framework\TestCase;
 
 class IsArrayRuleTest extends TestCase
 {
+    private IsArrayRule $rule;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->rule = new IsArrayRule();
+    }
 }
