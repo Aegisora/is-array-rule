de89c875868c26eaf233f41d2b40f638c004e2a2
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-05-07 20:23:47 +0400

    [AEGISORA-8] Implemented executeValidate.

--- src/IsArrayRule.php
+++ src/IsArrayRule.php
@@ -2,10 +2,19 @@
 
 namespace Aegisora\Rules;
 
-class IsArrayRule
+use Aegisora\RuleContract\Models\Context;
+use Aegisora\RuleContract\Models\Result;
+use Aegisora\RuleContract\Rule;
+
+class IsArrayRule extends Rule
 {
     public static function create(): self
     {
         return new self();
+    }
+
+    protected function executeValidate(Context $context): Result
+    {
+        return is_array($context->getValue()) ? $this->getDefaultValidResult() : $this->getDefaultInvalidResult();
     }
 }
