bae0125d2430cefaf248fcfe695258cc88d16b3f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-05-07 20:25:54 +0400

    [AEGISORA-8] Implemented assertActualResultEqualsExpected.

--- tests/Unit/IsArrayRuleTest.php
+++ tests/Unit/IsArrayRuleTest.php
@@ -2,6 +2,7 @@
 
 namespace Aegisora\Rules\Tests\Unit;
 
+use Aegisora\RuleContract\Models\Result;
 use Aegisora\Rules\IsArrayRule;
 use PHPUnit\Framework\TestCase;
 
@@ -14,5 +15,13 @@ class IsArrayRuleTest extends TestCase
         parent::setUp();
 
         $this->rule = new IsArrayRule();
+    }
+
+    private static function assertActualResultEqualsExpected(
+        Result $result,
+        array $expectedResult
+    ): void {
+        self::assertEquals($expectedResult['isValid'], $result->isValid());
+        self::assertEquals($expectedResult['failedRuleCode'], $result->getFailedRuleCode());
     }
 }
